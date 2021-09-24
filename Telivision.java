fcba889e49940c88b5e1257a198fedf4d69f9d0d
ambikaxwork <ambikatkotari4.xworkz@gmail.com>
2021-09-24 12:39:23 +0530

    first upload files

---
+++ Telivision.java
@@ -0,0 +1,38 @@
+class Telivision{
+	String name;
+	float inches;
+	float weight;
+	float price;
+	float height;
+	
+	Telivision(String name,float inches,float weight,float price,float height)
+	{
+		this.name=name;
+		this.inches=inches;
+		this.weight=weight;
+		this.price=price;
+		this.height=height;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ TelivisionRunner.java
@@ -0,0 +1,17 @@
+class TelivisionRunner{
+	public static void main(String[] maggi)
+	{
+		String name="samsung";
+		float inches=100.5f;
+		float weight=20.5f;
+		float price=35000;
+		float height=125;
+		Telivision tv=new Telivision(name,inches,weight,price,height);
+		System.out.println(tv.name);
+		System.out.println(tv.inches);
+		System.out.println(tv.weight);
+		System.out.println(tv.price);
+		System.out.println(tv.height);0
+
+	}
+}
